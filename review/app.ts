e681ed3e5e62eac3cc994d38d4e7bdfb3d48d45b
LNPappas <LPappas@gmail.com>
2023-02-03 14:03:04 -0800

    function types

--- review/app.ts
+++ review/app.ts
@@ -2,33 +2,11 @@
 // npm install --save-dev lite-server
 // add "start": "lite-server" in package.json, now run npm start
 
-enum resultType {
-  NUMBER,
-  TEXT,
-  NUMBER_AS_TEXT = 0,
+function generateError(message: string, code: number) {
+  throw {
+    message: message,
+    errorCode: code,
+  };
 }
 
-type Combinable = number | string;
-
-const combine = (
-  input1: Combinable,
-  input2: Combinable,
-  resultType: resultType
-) => {
-  let result: Combinable;
-  switch (resultType) {
-    case 0:
-      result = +input1 + +input2;
-      break;
-    case 1:
-      result = input1.toString() + input2.toString();
-      break;
-    default:
-      result = "default";
-  }
-  return result;
-};
-
-console.log(combine(30, 36, resultType.NUMBER));
-console.log(combine("L", "P", resultType.TEXT));
-console.log(combine("45", "27", resultType.NUMBER_AS_TEXT));
+generateError("Server Error", 501);
--- review/types.ts
+++ review/types.ts
@@ -37,3 +37,92 @@ console.log(person.roleLevel);
 console.log(RoleLevel.ADMIN);
 
 // union types
+enum resultType {
+  NUMBER,
+  TEXT,
+  NUMBER_AS_TEXT = 0,
+}
+
+type Combinable = number | string;
+
+const combine = (
+  input1: Combinable,
+  input2: Combinable,
+  resultType: resultType
+) => {
+  let result: Combinable;
+  switch (resultType) {
+    case 0:
+      result = +input1 + +input2;
+      break;
+    case 1:
+      result = input1.toString() + input2.toString();
+      break;
+    default:
+      result = "default";
+  }
+  return result;
+};
+
+console.log(combine(30, 36, resultType.NUMBER));
+console.log(combine("L", "P", resultType.TEXT));
+console.log(combine("45", "27", resultType.NUMBER_AS_TEXT));
+
+// function types
+const addFunc = (n1: number, n2: number): number => {
+  return n1 + n2;
+};
+
+const printAdd = (num: number): void => {
+  console.log("Result: " + num);
+};
+
+// Can save either addFunc or printAdd here
+let addValues: Function;
+
+// can only save printAdd here
+let printValues: (num: number) => void;
+addValues = addFunc;
+printValues = printAdd;
+
+const result = addValues(1, 2);
+printValues(result);
+
+addValues = printAdd;
+addValues(10);
+
+type CallBackType = (num: number) => number;
+
+const combineFunc = (n1: number, n2: number, cb: CallBackType): void => {
+  let res = n1 + n2;
+  cb(res);
+};
+
+const cb = (num: number): number => {
+  console.log("Callback Result: " + num);
+  return num;
+};
+
+combineFunc(3, 4, cb);
+
+// unknown type
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "LP";
+
+// cannot store string to unknown without type check
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+
+// never
+function generateError(message: string, code: number): never {
+  throw {
+    message: message,
+    errorCode: code,
+  };
+}
+
+generateError("Server Error", 500);
